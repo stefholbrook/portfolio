251be59b35da4419b5886a73dfb4c44cf8c460bb
Stefanie Holbrook <stefholbrook@gmail.com>
2016-04-02 18:19:30 +0000

    Add updated files and completed github portfolio

--- js/app.js
+++ js/app.js
@@ -1,11 +1,59 @@
 
 //toggle menu
-   $(document).ready(function($) {
+$(document).ready(function($) {
+       
     $('.toggle-nav').click(function(e) {
     $('.toggle-nav').toggleClass('active');
     $('.menu ul').toggleClass('active');
   
     e.preventDefault();
-});
+    });
+    
+    $('.portfolio li').click(function() {
+    var category = $(this).attr('class');
+
+    if (category==="h"){
+    	$('.angular, .jquery, .ruby').hide();
+    	$('.html').show(1000);		
+    }
+    else if(category==="j"){
+    	$('.angular, .ruby, .html').hide();
+    	$('.jquery').show(1000);
+    }
+    else if(category==="a"){
+    	$('.jquery, .ruby, .html').hide();
+    	$('.angular').show(1000);
+    }
+    else if(category==="r"){
+    	$('.jquery, .angular, .html').hide();
+    	$('.ruby').show(1000);
+    }
+    else if(category==="all"){
+    	$('.all').show(1000);
+    }
+    
+  });
+
+    
+    // $('form').submit(function() {
+    //     var email = $('#email').val();
+    // 	var subject = $('#subject').val();
+    // 	var message = $('#message').val();
+        
+    //     if(email === "") {
+    //     	$('.email-error').text("Please enter an email address");
+    //     }
+    	
+    //     if(subject === "") {
+    //     	$('.subject-error').text("Please enter a subject");
+    //     }
+    
+    //     if(message === "") {
+    //     	$('.message-error').text("Please enter a message");
+    //     }
+    	
+    //     return false;
+    // });
+
   
 });
